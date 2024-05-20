5d8b0021f235dea62827f9ffd536841e6ee7e819
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-05-20 16:01:30 -0700

    Npx create-next-app@latest, import custom favicon

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Min's Developer Portfolio",
+  description: "Welcome to my professional portfolio, a testament to my technical prowess and innovative approach to modern web development. Built with Next.js, Three.js, Framer Motion, and Tailwind CSS.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="mx-w-7xl w-full">
+        <h1>
+          Hello, Portfolio
+        </h1>
+      </div>
+    </main>
+  );
+}
